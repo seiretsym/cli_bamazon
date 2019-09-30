9e7d6ed676f766c055e92e29fa52c9c4a3d98396
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-30 16:43:56 -0700

    create database

---
+++ bamazonDB.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+    id INT NOT NULL AUTO_INCREMENT,
+    category VARCHAR(50) NULL,
+    price DECIMAL(10,2) NULL,
+    quantity INTEGER(10) NULL,
+);
