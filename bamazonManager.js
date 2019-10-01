ad912782bbf071c9e4cc21f0ea277a7905b35a1f
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-30 17:20:55 -0700

    add db info

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,6 +2,15 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// establish connection credentials and database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "derp",
+    database: "bamazonDB"
+})
+
 // init program by asking the user to make a choice
 function init() {
     inquirer.prompt([
